f828dd98349c859fc0e61eafb11b065107e2d0ab
Mac <macmitt2300@gmail.com>
2025-12-01 10:58:33 -0500

    Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Mackenzie Mittereder - Software Engineer",
@@ -18,7 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
