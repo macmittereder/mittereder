d3e09f3fc5dade35a2071f30f022e10956692474
Mac Mittereder <macmitt2300@gmail.com>
2026-04-09 07:56:58 -0400

    chore: security fixes with security headers and env examples

--- next.config.js
+++ next.config.js
@@ -4,6 +4,54 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
+
+// Security Headers
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google-analytics.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src 'self' blob: data:;
+  font-src 'self';
+  connect-src 'self' https://*.google-analytics.com;
+  frame-ancestors 'self';
+  base-uri 'self';
+  form-action 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+];
 
 module.exports = nextConfig;
